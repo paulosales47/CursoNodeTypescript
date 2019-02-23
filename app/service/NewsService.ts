3fbed022483fdc0c74a1df129f13362581d94371
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-23 17:51:25 -0300

    Finalizando operações basicas no service

--- app/service/NewsService.ts
+++ app/service/NewsService.ts
@@ -10,6 +10,18 @@ class NewsService{
         return NewsRepository.findById({_id});
     }
 
+    create(news){
+        return  NewsRepository.create(news);
+    }
+
+    update(_id, news){
+        return NewsRepository.findByIdAndUpdate(_id, news);
+    }
+
+    delete(_id){
+        return NewsRepository.findOneAndRemove(_id);
+    }
+
 }
 
 export default new NewsService();
