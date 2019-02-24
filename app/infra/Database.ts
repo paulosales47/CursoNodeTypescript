6cbdbd809ec382acb805f655e83e9e10d87d7c46
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-24 11:36:33 -0300

    Corrigindo problema no momento de publicação no Azure

--- app/infra/Database.ts
+++ app/infra/Database.ts
@@ -1,9 +1,10 @@
 import * as mongoose from 'mongoose';
 
 class Database{
-    private DB_URL = 'mongodb://mongo/portal_noticia';
+    private DB_URL = 'mongodb://conexao/portal_noticia';
 
     createConnection(){
+        console.log(process.env);
         mongoose.connect(this.DB_URL, {useNewUrlParser: true});
     }
 }
