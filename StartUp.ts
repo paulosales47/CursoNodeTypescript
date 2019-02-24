717015c6f246344abd3bfccc20ad30df78ba3054
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-24 00:08:38 -0300

    Configurando body-parser

--- StartUp.ts
+++ StartUp.ts
@@ -1,5 +1,7 @@
 import * as express from 'express'
 import Database from './app/infra/Database';
+import * as BodyParser from 'body-parser';
+import bodyParser = require('body-parser');
 
 class StartUp{
     public app: express.Application
@@ -17,6 +19,11 @@ class StartUp{
         this.app.route('/').get((requisicao, resposta)=> {
             resposta.send({versao: '0.0.1'});
         });
+    }
+
+    middler(){
+        this.app.use(BodyParser.json());
+        this.app.use(BodyParser.urlencoded({extended: false}));
     }
 }
 
