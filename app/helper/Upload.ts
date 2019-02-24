082cb6122cd5da4523182369eab7d920f53c1a83
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-24 15:26:38 -0300

    Criando middler para realização do upload

---
+++ app/helper/Upload.ts
@@ -0,0 +1,14 @@
+import * as multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function(requisicao, file, callback){
+        callback(null, "/uploads");
+    },
+    filename: function(requisicao, file, callback){
+        callback(null, file.originalname);
+    }
+});
+
+const uploads = multer({storage: storage});
+
+export default uploads;
