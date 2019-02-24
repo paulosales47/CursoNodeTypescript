ccaace9e01f806d2d9a447bce217ca1585f6d364
Paulo Henrique Sales Sampaio <paulohenrique.sales47@gmail.com>
2019-02-24 08:37:18 -0300

    Conterizando projeto e configurando docker-compose para acesso a base de dados

--- app/infra/Database.ts
+++ app/infra/Database.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 class Database{
-    private DB_URL = 'mongodb://localhost:27017/portal_noticia';
+    private DB_URL = 'mongodb://mongo/portal_noticia';
 
     createConnection(){
         mongoose.connect(this.DB_URL, {useNewUrlParser: true});
--- docker.md
+++ docker.md
@@ -1,3 +1,7 @@
 * docker pull mongo
 
 * docker run -d -p 27017:27017 -p 28017:28017 -e --noauth mongo
+
+* docker build -t paulohsampaio/nodets .
+
+* Docker-compose up
